df7e40865f504e2e2ff95ebe6ba4d5e8c4d6e5dd
zhaoguoqi <zhaoguoqi@sjtu.edu.cn>
2016-04-09 15:15:01 +0800

    学习了InputStreamReader 和BufferedReader来将键盘输入用来构造学生

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -10,14 +10,26 @@ public class Student{
     private String sname;
     private int sage;
 
+    //将键盘中的输入送到输入流里,在把输入流阅读器放到缓冲里
+    private InputStreamReader isr = new InputStreamReader(System.in);
+    private BufferedReader br = new BufferedReader(isr);
+
+
     public Student(String i,String n,int a){
         sid = i;
         sname = n;
         sage = a;
     }
-    public Student(){
 
+    public Student() throws IOException{
+        System.out.print("请输入学号:");
+        sid = br.readLine();
+        System.out.print("请输入姓名:");
+        sname = br.readLine();
+        System.out.print("请输入年龄:");
+        sage = Integer.parseInt(br.readLine());//将键盘的输入的字符串转化成整数
     }
+
     public void input(String i,String n,int a){
         sid =i;
         sname = n;
@@ -44,15 +56,15 @@ public class Student{
         System.out.println("sage="+sage);
     }
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
         Student s1 = new Student();
-        s1.input("s01","李小龙",32);
+//        s1.input("s01","李小龙",32);
         s1.judge();
         s1.output();
         Student s2 = new Student("s02","张三丰",45);
         s2.judge();
         s2=null;
-        System.gc();//使用gc来调用finalize函数
+        System.gc();//使用gc来调用finalize函数,finalize无法控制执行流程
 
     }
 }
