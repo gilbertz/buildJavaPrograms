f4b7aab812d13fcf8c9f24c29265a1c0e8288de8
zhaoguoqi <zhaoguoqi@sjtu.edu.cn>
2016-04-09 14:33:16 +0800

    添加了构造函数和gc的finalize函数

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -10,6 +10,14 @@ public class Student{
     private String sname;
     private int sage;
 
+    public Student(String i,String n,int a){
+        sid = i;
+        sname = n;
+        sage = a;
+    }
+    public Student(){
+
+    }
     public void input(String i,String n,int a){
         sid =i;
         sname = n;
@@ -30,11 +38,21 @@ public class Student{
         System.out.println("sage="+sage);
     }
 
+    protected final void  finalize(){
+        System.out.println("sid="+sid);
+        System.out.println("sname="+sname);
+        System.out.println("sage="+sage);
+    }
+
     public static void main(String [] args){
         Student s1 = new Student();
         s1.input("s01","李小龙",32);
         s1.judge();
         s1.output();
+        Student s2 = new Student("s02","张三丰",45);
+        s2.judge();
+        s2=null;
+        System.gc();//使用gc来调用finalize函数
 
     }
 }
